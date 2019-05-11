92c5da7cfd286ec75c0172eddae18ecee120bcbb
doubletcjs <doubletcjs@gmail.com>
2019-05-11 16:21:11 +0800

    开启kotlin

--- Kotlin/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Kotlin/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,9 +7,9 @@ import android.view.Window
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
     }
 }
