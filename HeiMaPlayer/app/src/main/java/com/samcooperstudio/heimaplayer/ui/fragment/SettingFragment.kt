4b7ad892fd11c201dbbc8ecf44870619aa94b9b5
sam <doubletcjs@gmail.com>
2019-07-26 20:41:35 +0800

    PreferenceScreen 去除左边空白

--- HeiMaPlayer/app/src/main/java/com/samcooperstudio/heimaplayer/ui/fragment/SettingFragment.kt
+++ HeiMaPlayer/app/src/main/java/com/samcooperstudio/heimaplayer/ui/fragment/SettingFragment.kt
@@ -1,21 +1,59 @@
 package com.samcooperstudio.heimaplayer.ui.fragment
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.preference.Preference
-import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.*
+import android.support.v7.widget.RecyclerView
 import com.samcooperstudio.heimaplayer.R
 import com.samcooperstudio.heimaplayer.ui.activity.AboutActivity
 
 class SettingFragment: PreferenceFragmentCompat() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.setting)
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        addPreferencesFromResource(R.xml.setting)
+//    }
+
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        return inflater.inflate(R.layout.setting_screen, container, false)
+//        addPreferencesFromResource(R.xml.setting)
+//    }
 
     override fun onCreatePreferences(p0: Bundle?, p1: String?) {
+        setPreferencesFromResource(R.xml.setting, p1)
+    }
 
+    private fun setAllPreferencesToAvoidHavingExtraSpace(preference: Preference) {
+        preference.isIconSpaceReserved = false
+        if (preference is PreferenceGroup) {
+            for (i in 0 until preference.preferenceCount) {
+                setAllPreferencesToAvoidHavingExtraSpace(preference.getPreference(i))
+            }
+        }
+    }
+
+    override fun setPreferenceScreen(preferenceScreen: PreferenceScreen?) {
+        if (preferenceScreen != null) {
+            setAllPreferencesToAvoidHavingExtraSpace(preferenceScreen)
+        }
+
+        super.setPreferenceScreen(preferenceScreen)
+    }
+
+    override fun onCreateAdapter(preferenceScreen: PreferenceScreen?): RecyclerView.Adapter<*> {
+        object: PreferenceGroupAdapter(preferenceScreen) {
+            @SuppressLint("RestrictedApi")
+            override fun onPreferenceHierarchyChange(preference: Preference?) {
+                if (preference != null) {
+                    setAllPreferencesToAvoidHavingExtraSpace(preference)
+                }
+
+                super.onPreferenceHierarchyChange(preference)
+            }
+        }
+
+        return super.onCreateAdapter(preferenceScreen)
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
