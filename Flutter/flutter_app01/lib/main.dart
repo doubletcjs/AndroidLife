c333d60a60b3f1981535048160a425bbe78c5012
doubletcjs <doubletcjs@gmail.com>
2019-08-12 17:19:54 +0800

    update

---
+++ Flutter/flutter_app01/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+ runApp(MyApp());
+}
+
+//自定义组件
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        "测试11122",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 20.0,
+          // color: Colors.red,
+          color: Color.fromRGBO(244, 233, 121, 0.5),
+        ),
+      ),
+    );
+  }
+   
+}
